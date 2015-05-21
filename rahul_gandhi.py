65532f14b32311fe8e5a5dee8d3a77931486d6d2
Meet Shah <meetshah1995@iitb.ac.in>
2015-05-22 01:31:02 +0800

    Add Rahul Gandhi

---
+++ rahul_gandhi.py
@@ -0,0 +1,24 @@
+from __past__ import dumbness
+import empowerment from women
+
+'''
+	This module was under a heavy no-maintainence mode in the recent past 
+	where it travelled and isolated itself from the world.
+	
+	Update 1.1: The module is recently bak from no-maintenence mode, but 
+	due to past errant behaviour please use at your dicretion.  
+'''
+
+def answerQuestion( ''' Question is irrelavant so need to add it as a parameter''' ):
+	string_answer = womentEmpowerizeSentence() + rahulGanduEkSochHaiBullshit()
+	string_answer += getNonsenseExampleFromIrrelvantPlace()
+	return string_answer
+
+def interviewMode(question_set):
+	for questions in question_set:
+		answerQuestion()
+		facePalm(Sonia)
+		Congress.votes -= 10000
+		BJP.members.LOLmode = true
+
+
