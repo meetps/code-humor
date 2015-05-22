2eb5f4e3091b054e2811d1b7c7a5de74639f9353
Meet Shah <meetshah1995@iitb.ac.in>
2015-05-22 15:49:07 +0800

    Added Narendra Modi

---
+++ narendra_modi.cpp
@@ -0,0 +1,51 @@
+#include<OratorSkills>
+#include<Chai>
+
+#include "Reliance_Profit_Updates.h"
+#include "Adani_Profit_Updates.h" 
+
+string Bhashan(latlng Location, string scriptWritersScript, string joke)
+	{
+		if(Location != India)
+			{
+				string speech = getGenericStupidPraise(getNation(Location));
+				speech += AppraiseSelfWith(countymen.ashamedBeforeMyArrival());
+				return inDesiAccentBrokenEnglish(speech) + selfPublicity();
+			}
+		/* Else is Currently Commented as not needed as the caller class is on World 
+		   Tour for an indefnite period. The return of  the caller class may take a 
+                   long a time. Maybe We will see the hailey's comet and the caller class at
+	           the same time back in India. 
+		else 
+			{
+				if(event == electionRally && getRulingParty(event.Location) != BJP)
+					{
+						string speech = generateBaselessNonSenseCriticism(getRulingParty(event.location);
+						speech += makeJokeOn(opponent.physical,candidate.education);
+						speech.addNationalistTadka();
+						speech.garnishWith(opponent.BaselessConnectionsWithPaK);
+						while(!crowdCheering())
+							{
+								return speech.serveHot();
+							}	 
+					}
+			}
+	}
+*/
+
+int main()
+{
+	while(nextElectionDayRemaining > 60)
+		{
+			Bhashan();
+			Publicity(Bhashan);
+			string khass_mitra[N] = {Reliance, Adani, other(party_fund_raisers)};
+			MakeBillsToProfit(khass_mitra[N]);
+			WishBudday(RandomCelebrity());
+			AttendWedding(RandomCelebrity());
+			/* A lot ot memers from the ignore_list were removed as they generated
+			problem_solution_generates_no_votebank Exception. Others to be removed
+			 soon*/
+			ignore_list = {social_issues, flaws_in_ministries, farmer_problems}
+		}	
+} 
